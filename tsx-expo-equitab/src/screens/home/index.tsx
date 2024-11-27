e5ac076f765efbce706ee43df7bde90d1fb6443b
zS1L3NT Mac <dev@zectan.com>
2024-11-28 05:23:51 +0800

    ✨ customise home tab navigator

--- tsx-expo-equitab/src/screens/home/index.tsx
+++ tsx-expo-equitab/src/screens/home/index.tsx
@@ -6,7 +6,15 @@ const Tab = createMaterialTopTabNavigator()
 
 export default function HomeScreen() {
 	return (
-		<Tab.Navigator initialRouteName="Direct Tabs">
+		<Tab.Navigator
+			initialRouteName="Direct Tabs"
+			screenOptions={{
+				tabBarScrollEnabled: true,
+				tabBarLabelStyle: { fontWeight: "600" },
+				tabBarItemStyle: { width: "auto" },
+				tabBarIndicatorStyle: { width: 0.6 },
+			}}
+		>
 			<Tab.Screen name="Direct Tabs" component={DirectTab} />
 			<Tab.Screen name="Group Tabs" component={GroupTab} />
 		</Tab.Navigator>
