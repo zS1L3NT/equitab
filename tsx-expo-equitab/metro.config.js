c3a15522ed0420335438205a78508d2030c13912
zS1L3NT Mac <dev@zectan.com>
2024-11-29 01:40:13 +0800

    ✨ add nativewind to app

--- tsx-expo-equitab/metro.config.js
+++ tsx-expo-equitab/metro.config.js
@@ -1,7 +1,8 @@
 const { getDefaultConfig } = require("expo/metro-config")
+const { withNativeWind } = require("nativewind/metro")
 
 const config = getDefaultConfig(__dirname)
 
 config.resolver.unstable_enablePackageExports = true
 
-module.exports = config
+module.exports = withNativeWind(config, { input: "./src/global.css" })
