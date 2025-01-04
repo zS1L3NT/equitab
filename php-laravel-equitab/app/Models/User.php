1124f02c612b6653d059d27b6ab790457d1dffdd
zS1L3NT Mac <dev@zectan.com>
2025-01-05 02:41:56 +0800

    ✨ add profile viewing and editing

--- php-laravel-equitab/app/Models/User.php
+++ php-laravel-equitab/app/Models/User.php
@@ -3,11 +3,24 @@
 namespace App\Models;
 
 use App\Enums\FriendshipStatus;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Http\UploadedFile;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Carbon;
 use Laravel\Sanctum\HasApiTokens;
 
+/**
+ * @property int $id
+ * @property string $username
+ * @property string $phone_number
+ * @property Carbon|null $phone_number_verified_at
+ * @property string|null $picture_path
+ * @property string $password
+ *
+ * @property Collection<User> $friends
+ */
 class User extends Authenticatable
 {
     use HasApiTokens, Notifiable;
@@ -29,6 +42,16 @@ class User extends Authenticatable
         'password' => 'hashed',
     ];
 
+    public function getPicturePathAttribute(): string|null
+    {
+        return $this->attributes['picture_path'] ? asset($this->attributes['picture_path']) : null;
+    }
+
+    public function setPictureFileAttribute(string|UploadedFile $picture): void
+    {
+        $this->attributes['picture_path'] = $picture instanceof UploadedFile ? '/' . $picture->storePubliclyAs('images/users/' . $this->username . '.png') : $picture;
+    }
+
     public function friendships(): HasMany
     {
         $sent = $this->hasMany(Friendship::class, 'to_user_id');
@@ -36,4 +59,12 @@ class User extends Authenticatable
         return $sent->union($received)->latest();
     }
 
+    public function getFriendsAttribute()
+    {
+        return $this
+            ->friendships()
+            ->where('status', FriendshipStatus::Accepted)
+            ->get()
+            ->map(fn($f) => $f->other($this));
+    }
 }
