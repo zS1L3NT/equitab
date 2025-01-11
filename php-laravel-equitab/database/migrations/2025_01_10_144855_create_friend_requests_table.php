f78aff0bf04d117bcba0f6ae05d99c803844ca7c
zS1L3NT Mac <dev@zectan.com>
2025-01-11 15:31:12 +0800

    ✨ finish friendship api requests

--- php-laravel-equitab/database/migrations/2025_01_10_144855_create_friend_requests_table.php
+++ php-laravel-equitab/database/migrations/2025_01_10_144855_create_friend_requests_table.php
@@ -1,23 +1,23 @@
 <?php
 
-use App\Enums\FriendshipStatus;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('friendships', function (Blueprint $table) {
+        Schema::create('friend_requests', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(\App\Models\User::class, 'user_id')->constrained();
-            $table->foreignIdFor(\App\Models\User::class, 'friend_id')->constrained();
+            $table->foreignIdFor(\App\Models\User::class, 'from_user_id')->constrained();
+            $table->foreignIdFor(\App\Models\User::class, 'to_user_id')->constrained();
             $table->timestamp('created_at')->useCurrent();
 
-            $table->unique(['user_id', 'friend_id']);
+            $table->unique(['from_user_id', 'to_user_id']);
         });
     }
 
@@ -26,6 +26,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('friendships');
+        Schema::dropIfExists('friend_requests');
     }
 };
--- php-laravel-equitab/routes/api.php
+++ php-laravel-equitab/routes/api.php
@@ -3,16 +3,22 @@
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'v1'], function () {
-    Route::group(['prefix' => 'auth'], function () {
-        Route::post('login', [\App\Http\Controllers\Api\AuthController::class, 'login']);
+    Route::post('auth/login', [\App\Http\Controllers\Api\AuthController::class, 'login']);
 
-        Route::group(['middleware' => 'auth:sanctum'], function () {
-            Route::post('logout', [\App\Http\Controllers\Api\AuthController::class, 'logout']);
+    Route::group(['middleware' => 'auth:sanctum'], function () {
+        Route::post('auth/logout', [\App\Http\Controllers\Api\AuthController::class, 'logout']);
+
+        Route::singleton('user', \App\Http\Controllers\Api\UserController::class)->except('edit');
+
+        Route::group(['prefix' => 'friends'], function() {
+            Route::get('/', [\App\Http\Controllers\Api\FriendController::class, 'index']);
+            Route::delete('{friend}', [\App\Http\Controllers\Api\FriendController::class, 'destroy']);
+
+            Route::group(['prefix' => 'requests'], function() {
+                Route::get('/', [\App\Http\Controllers\Api\FriendRequestController::class, 'index']);
+                Route::post('{friend}', [\App\Http\Controllers\Api\FriendRequestController::class, 'store']);
+                Route::delete('{friend}', [\App\Http\Controllers\Api\FriendRequestController::class, 'destroy']);
+            });
         });
-    });
-
-    Route::group(['prefix' => 'user', 'middleware' => 'auth:sanctum'], function () {
-        Route::get('/', [\App\Http\Controllers\Api\UserController::class, 'show']);
-        Route::put('/', [\App\Http\Controllers\Api\UserController::class, 'update']);
     });
 });
